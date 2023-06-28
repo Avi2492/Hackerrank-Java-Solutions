1ea41050e4d1a6d67706a553a195975ff8420e3f
AVINASH PANDEY <98386256+Avi2492@users.noreply.github.com>
2023-06-28 20:24:25 +0530

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,39 @@
+// Task
+// Create a class called BankAccount with two constructors and two methods
+// First constructor with one String parameter: holdername
+// Second constructor with one parameter: balance
+// Create 2 methods for displaying holdername and balance respectively
+// Display the holdername of the 1st BankAccount object with holdername "John".
+// Display the balance of the 2nd BankAccount with balance:- 1000.
+
+// Update the '_' to solve the problem
+class BankAccountOne {
+    double balance;
+    String holdername;
+    
+    public BankAccount(String n) {
+        holdername = n;
+    }
+    
+    public BankAccount(double b) {
+        balance = b;
+    }
+    
+    void displayName() {
+        System.out.println("Account holder name: " + holdername);       
+    }
+    
+    void displayBalance() {
+
+        System.out.println("Balance: $" + balance);
+    }
+}
+
+class BankAccount {
+    public static void main(String[] args) {   
+        BankAccount b1 = new BankAccount("John");
+        BankAccount b2 = new BankAccount(1000);
+        b1.displayName();
+        b2.displayBalance();
+    }
+}
